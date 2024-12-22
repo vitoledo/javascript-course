a2ad8aeb8d28249fb00b98f11d8065c7d3608e4b
vitoledo <146836522+vitoledo@users.noreply.github.com>
2024-12-22 15:27:22 -0300

    aula 11

---
+++ aula11/ex08.js
@@ -0,0 +1,6 @@
+var vel = 50
+console.log(`a velocidade do seu carro é ${vel}Km/h`)
+if(vel > 60){
+    console.log('você ultrapassou a velocidade permitida')
+}
+console.log('dirija sempre usando o cinto de segurança')
---
+++ aula11/ex09.js
@@ -0,0 +1,6 @@
+var pais = 'EUA'
+if(pais == 'Brasil'){
+    console.log('você é brasileiro')
+}else{
+    console.log('você é extrangeiro')
+}
